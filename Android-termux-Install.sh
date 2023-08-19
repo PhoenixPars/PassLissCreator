5b171bc48f81a3e600a1fee506c8315d5a7f1d30
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-19 16:30:57 +0330

    Update Android-termux-Install.sh
    Add uldated

--- Android-termux-Install.sh
+++ Android-termux-Install.sh
@@ -7,5 +7,19 @@ echo "
 | Version : 1.0 BETA                   |
 |______________________________________|
 "
-echo "\n"
+echo "/n"
 read -p " Do you allow us to update the system? [y/n] ": AcsseccSysUpdate
+
+if [[ "${AcsseccSysUpdate}" == "y"]]; then
+apt-get update
+apt-get upgrade
+elif [[ "${AcsseccSysUpdate}" == "n"]]; then
+
+elif [[ "${AcsseccSysUpdate}" == "Y"]]; then
+apt-get update
+apt-get upgrade
+elif [[ "${AcsseccSysUpdate}" == "N"]]; then 
+
+else 
+echo "please enter an option"
+fi
