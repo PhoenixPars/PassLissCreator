5ffff481e6392643d0276d3df4fc6046b56c5a4f
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-20 18:18:49 +0330

    Update Android-termux-Install.sh
    Add instalation python

--- Android-termux-Install.sh
+++ Android-termux-Install.sh
@@ -8,20 +8,8 @@ echo "
 | Version : 1.0 BETA                   |
 |______________________________________|
 "
-read -p " Do you allow us to update the system? [y/n] ": AcsseccSysUpdate
-
-if [["${AcsseccSysUpdate}" == "y"]]; then
+read -p " Do you allow us to update the system? [y/n] ": AcsseccSysUpdate 
 apt-get update
 apt-get upgrade
 read -p " Do you allow us to update or install python? [y/n] ": AcsseccPyInnstall
-elif [["${AcsseccSysUpdate}" == "n"]]; then
-read -p " Do you allow us to update or install python? [y/n] ": AcsseccPyInnstall
-elif [["${AcsseccSysUpdate}" == "Y"]]; then
-apt-get update
-apt-get upgrade
-read -p " Do you allow us to update or install python? [y/n] ": AcsseccPyInnstall
-elif [["${AcsseccSysUpdate}" == "N"]]; then 
-read -p " Do you allow us to update or install python? [y/n] ": AcsseccPyInnstall
-else 
-echo "please enter an option"
-fi
+pkg install python 
