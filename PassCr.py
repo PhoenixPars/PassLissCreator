2a6e2fbac34c2f40c6017c8eefd7695efd27c565
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-20 20:45:05 +0330

    Create PassCr.py

---
+++ PassCr.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("clear")
+print("________________________________________________________")
+print("|             Welcome to List Password Creator          |")
+print("|             my telegram channel : PhoenixPars         |")
+print("|-------------------------------------------------------|")
+print("| 1 . Create Password List with Numbers                 |")
+print("| 2 . Create Password List with Word                    |")
+print("| 3 . Create Password List with information             |")
+print("| 4 . Create Password List by combining modes 1, 2 and 3|")
+print("|-------------------------------------------------------|")
+print("| 5 . buy VIP Password list                             |")
+print("| 6 . show Creator informatio                           |")
+print("| 7 . exit                                              |")
+print("|-------------------------------------------------------|")
+
+
