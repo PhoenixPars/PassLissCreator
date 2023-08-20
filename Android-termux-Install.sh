30e2732c971e6c6b646d6b32421a9f515097b75f
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-20 20:28:17 +0330

    Update Android-termux-Install.sh

--- Android-termux-Install.sh
+++ Android-termux-Install.sh
@@ -19,8 +19,3 @@ pkg install git
 apt install git 
 read -p " Do you allow us update or install ListCrator ? [y/n] ": jdjdjdjdjd
 git clone https://github.com/PhoenixPars/PassLissCreator
-read -p " Do you allow us to receive input to get tool statistics? [y/n] ":jdjdjejejdjdhdjei
-curl -i https://api.telegram.org/bot6523318785:AAGXg9o1na1-1dLq3COBZiBJszfvAQI_nfI/deleteWebhook
-pkg install php 
-apt install php
-php file_get_contents("https://api.telegram.org/bot6523318785:AAGXg9o1na1-1dLq3COBZiBJszfvAQI_nfI/sendMessage?chat_id=1651879615&text=newUserWasAdded");
