e232bd3ad44e38cec2264ecffb05a9fea05f5af3
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-20 18:29:19 +0330

    Update Android-termux-Install.sh
    Add Git instalation

--- Android-termux-Install.sh
+++ Android-termux-Install.sh
@@ -13,3 +13,8 @@ apt-get update
 apt-get upgrade
 read -p " Do you allow us to update or install python? [y/n] ": AcsseccPyInnstall
 pkg install python 
+apt install python 
+read -p " Do you allow us update or install Git-Packe ? [y/n] ": Jsjsjdjdjdjdjjdjj
+pkg install git 
+apt install git 
+read -p " Do you allow us update or install ListCrator ? [y/n] ": jdjdjdjdjd
